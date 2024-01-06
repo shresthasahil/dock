7e00cbc1ad0b6534836302a6be7bd4da81639174
neupaneprashant22 <55069800+neupaneprashant22@users.noreply.github.com>
2024-01-06 20:14:54 +0545

    Update init.sql

--- all_in_docker/init.sql
+++ all_in_docker/init.sql
@@ -8,3 +8,16 @@ CREATE TABLE IF NOT EXISTS employees(
     position VARCHAR(50), 
     wage FLOAT NOT NULL
 );
+CREATE USER 'test'@'localhost' IDENTIFIED BY 'mauFJcuf5dhRMQrjj';
+GRANT ALL PRIVILEGES ON employeeSystem.* TO 'test'@'localhost';
+
+-- Create the user and grant privileges for any IP
+CREATE USER 'test'@'%' IDENTIFIED BY 'mauFJcuf5dhRMQrjj';
+GRANT ALL PRIVILEGES ON employeeSystem.* TO 'test'@'%';
+
+FLUSH PRIVILEGES;
+
+ALTER USER 'test'@'localhost' IDENTIFIED WITH 'mysql_native_password' BY 'mauFJcuf5dhRMQrjj';
+ALTER USER 'test'@'%' IDENTIFIED WITH 'mysql_native_password' BY 'mauFJcuf5dhRMQrjj';
+FLUSH PRIVILEGES;
+

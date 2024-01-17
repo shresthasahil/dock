27fc5b20281e39e65d1a91fd4bccf560ef9f940e
Samil <sshrestha@deerhold.com>
2024-01-17 14:07:38 +0545

    ipupdate

--- all_in_docker/init.sql
+++ all_in_docker/init.sql
@@ -2,6 +2,7 @@ CREATE DATABASE IF NOT EXISTS employeeSystem;
 USE employeeSystem;
 
 CREATE TABLE IF NOT EXISTS employees(
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL, 
     age INT unsigned, 
     country VARCHAR(50), 
